4d80b0960461af0a2f16feda59b8b0416097e788
kiethohoai <kietho.060193@gmail.com>
2024-10-28 08:18:50 +0700

    010 Refactoring for Project Architecture

--- script.js
+++ script.js
@@ -13,72 +13,88 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-//Geolocation API
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    // Success to get current location
-    function (position) {
-      const { latitude, longitude } = position.coords;
+//todo APP
+class App {
+  #map;
+  #mapEvent;
 
-      // console.log(`🚀  latitude, longitude =>`, latitude, longitude);
-      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  constructor() {
+    this._getPosition();
 
-      // Current location marker
-      const coords = [latitude, longitude];
+    // Handle Form Submit
+    form.addEventListener('submit', this._newWorkout.bind(this));
 
-      // Leaflet map
-      map = L.map('map').setView(coords, 13);
+    // Handle Select on InputType
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`Could not get your current position!`);
+        },
+      );
+    }
+  }
 
-      L.marker(coords).addTo(map).bindPopup('My Location').openPopup();
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
 
-      // Handle Click On Map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    // Fail to get current position
-    function () {
-      alert(`Could not get your current position!`);
-    },
-  );
+    // Current location marker
+    const coords = [latitude, longitude];
+
+    // Leaflet map
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    L.marker(coords).addTo(this.#map).bindPopup('My Location').openPopup();
+
+    // Handle Click On Map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    // Prevent default behavior of the form
+    e.preventDefault();
+
+    // Clear all input fields
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputCadence.value = '';
+    inputElevation.value = '';
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }),
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
-// FORM HANDLER
-form.addEventListener('submit', function (e) {
-  // Prevent default behavior of the form
-  e.preventDefault();
-
-  // Clear all input fields
-  inputDistance.value = '';
-  inputDuration.value = '';
-  inputCadence.value = '';
-  inputElevation.value = '';
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      }),
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-// Handle Select on InputType
-inputType.addEventListener('change', (e) => {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
